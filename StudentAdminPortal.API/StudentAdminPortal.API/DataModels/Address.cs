650d248df4ff14ff2fbc403b013fac8602292574
Be-Asia <amerigirl29@gmail.com>
2023-06-01 13:50:22 -0500

    add connection string to appsettings

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/DataModels/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentAdminPortal.API.DataModels
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string PhysicalAddess { get; set; }
+        public string PostalAddress { get; set; }
+
+        //Navigation Property
+
+        public Guid StudentId { get; set; }
+
+    }
+}

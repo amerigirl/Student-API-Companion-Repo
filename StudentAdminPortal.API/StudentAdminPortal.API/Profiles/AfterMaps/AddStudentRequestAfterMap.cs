3939f3072ef5b7fccdc2123f710fae13369a6071
Be-Asia <amerigirl29@gmail.com>
2023-09-15 15:16:56 -0500

    took out description and genderId requirement for addStudent method

--- StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AfterMaps/AddStudentRequestAfterMap.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AfterMaps/AddStudentRequestAfterMap.cs
@@ -9,12 +9,18 @@ namespace StudentAdminPortal.API.Profiles.AfterMaps
           public void Process(AddStudentRequest source, DataModels.Student destination, ResolutionContext context)
         {
             destination.Id = Guid.NewGuid();
+
             destination.Address = new DataModels.Address()
             {
                 Id = Guid.NewGuid(),
                 PhysicalAddress = source.PhysicalAddress,
                 PostalAddress = source.PostalAddress,
-            }; 
+            };
+
+            destination.Gender = new DataModels.Gender()
+            {
+                Id = Guid.NewGuid()
+            };
         
         }
     }
